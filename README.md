c235f2a033425b59d562f2dc87322a530be8e15a
Spotchi <quentin-laurent93@hotmail.com>
2017-12-03 20:51:44 +0000

    Added an add function for the database, struggling with the hashing mechanism(getting the same hash)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # dETaiL
+
+dETaiL is a project that aims at taking away the hassle from tuning different combinations of ETL pipelines
+
+It increases speed of computation by simply loading the results that were previously computed from disk.
+
+
+Setup
+
+Ubuntu 16.04:
+-------------
+
+Pip requirements
+
+Set up MongoDB
+++++++++++++++
+
+Follow the instructions in the following tutorial
+
+https://www.howtoforge.com/tutorial/install-mongodb-on-ubuntu-16.04/#install-mongodb-on-ubuntu-
+
+
+
--- mydb.py
+++ mydb.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 import pymongo
 import hashlib
@@ -16,13 +17,26 @@ data_folder = 'data'
 #    '''
 
 def known_hash(hash_value):
-    if coll.find_one({'config_hash': hash_value}):
+    hval = hash_value.hexdigest()
+    if coll.find_one({'config_hash': hval}):
         return True
 
     return False 
     
 def load(hash_value):
-   
-    config = coll.find_one({'config_hash': hash_value})
+    hval = hash_value.hexdigest()
+    config = coll.find_one({'config_hash': hval})
     filename = os.path.join(data_folder, hash_value)
+    print('Loading ', hval)
     return pickle.load(open(filename, "rb")) 
+
+def add(hash_value, config, obj_value):
+    hval = hash_value.hexdigest()
+    print(type(hval))
+    post = {'config_hash': hval}
+   
+    post_id = coll.insert_one(post).inserted_id  
+    filename = os.path.join(data_folder, hval)
+    pickle.dump(obj_value, open(filename, "wb")) 
+
+    print('Value inserted at index {} for value {}', hval, post_id)
